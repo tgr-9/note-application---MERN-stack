ec17d857ca61c7368360a352d82f40c6927809af
tgr-9 <top.gru.999@gmail.com>
2024-06-19 18:50:50 +0530

    added express validater

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const User = require('../models/Users');
+const { body, validationResult } = require('express-validator');
 
 const router = express.Router();
 
-router.post('/', (req,res) => {
-    console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    res.send(req.body);
-})
+router.post('/',[
+    body('name','Enter a valid name').isLength({ min: 3 }),
+    body('email','email is invalid!').isEmail(),
+    body('password','Password must be atleast 8 characters').isLength({ min: 8 })
+], (req,res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+      }).then(user => { 
+        console.log("message: User created successfully!!");
+        res.json(user);
+    })
+      .catch(err => {
+        console.log("error: ",err);
+        res.json({error: "email already exists!!"});
+      });
+});
 
 module.exports = router;
