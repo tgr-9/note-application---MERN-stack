c840335757beff6d4e2682177f9013c1620d7e25
tgr-9 <top.gru.999@gmail.com>
2024-06-19 17:29:39 +0530

    setting up models and routes

--- backend/index.js
+++ backend/index.js
@@ -11,11 +11,14 @@ const port = 5000;
 
 
 // create routes
-app.get('/', (req,res) => {
-    res.send("Hello the-zoomeee");
-})
+app.get('/',(req,res) => {
+    res.send('Server is up and running !!');
+});
+
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/notes', require('./routes/notes'));
 
 // starting the express server
 app.listen(port, ()=> {
     console.log(`my-notebook backend listening at http://localhost:${port}`);
-})
+});
