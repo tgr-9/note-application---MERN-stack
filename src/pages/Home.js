3b2875f072cf02104fb4140e4e48a6995002fa0a
tgr-9 <top.gru.999@gmail.com>
2024-06-23 22:57:54 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import Notes from "../components/Notes";
 
 function Home() {
-  document.title = "Home - My Notebook";
+  document.title = "MyNottebok - Home";
   return (
     <div>
       <Notes />
