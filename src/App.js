ad88f53b655a5353044ed5cc0dbc12d13d7fab99
tgr-9 <top.gru.999@gmail.com>
2024-06-23 13:19:48 +0530

    added settings feature

--- src/App.js
+++ src/App.js
@@ -12,33 +12,30 @@ import NoteState from "./context/notes/noteState";
 import Signup from "./components/Auth/Signup";
 import Settings from "./components/Settings"; // Import the Settings component
 import ProtectedRoute from "./components/ProtectedRoute"; // Import the ProtectedRoute component
-import ThemeState from "./context/theme/themeState"; // Import the ThemeState
 
 function App() {
   return (
     <>
       <AlertState>
         <NoteState>
-          <ThemeState>
-            <Router>
-              <Navbar />
-              <Alert />
-              <div className="container">
-                <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/about" element={<About />} />
-                  <Route path="/aboutUs" element={<AboutUS />} />
-                  <Route path="/login" element={<Login />} />
-                  <Route path="/signup" element={<Signup />} />
-                  <Route path="/settings" element={
-                    <ProtectedRoute>
-                      <Settings />
-                    </ProtectedRoute>
-                  } />
-                </Routes>
-              </div>
-            </Router>
-          </ThemeState>
+          <Router>
+            <Navbar />
+            <Alert />
+            <div className="container">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/aboutUs" element={<AboutUS />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/settings" element={
+                  <ProtectedRoute>
+                    <Settings />
+                  </ProtectedRoute>
+                } />
+              </Routes>
+            </div>
+          </Router>
         </NoteState>
       </AlertState>
     </>
